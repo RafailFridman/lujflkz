8416b014f50ab39db1c385db7f888d1934729f84
omerbartal <omer234@gmail.com>
2022-03-04 22:28:43 +0200

    add captions_imgs.txt

--- change_files.py
+++ change_files.py
@@ -26,13 +26,13 @@ image_dirs = get_all_dirs(base_dir)
 command = ''
 for image_dir in image_dirs:
     src_image_path = get_image_path(image_dir)
-    shutil.copy(src_image_path, os.path.join('src_imgs', f'{i}.jpg'))
     # img = Image.open(src_image_path)
     # w, h = img.size
     # if min(w, h) > 512:
     #     img = T.Resize(512)(img)
     #     w, h = img.size
     for edit_prompt in get_all_edit_prompts(image_dir):
+        shutil.copy(src_image_path, os.path.join('src_imgs', f'{i}.jpg'))
         command += edit_prompt + '\n'
         # dir = os.path.join(image_dir, edit_prompt)
         # ours = [os.path.join(dir, f) for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) and os.path.join(dir, f).endswith('png') and 'vqgan' not in f and 'styler' not in f][0]
@@ -57,5 +57,5 @@ for image_dir in image_dirs:
         # output_path = os.path.join(image_dir, edit_prompt, 'vqgan.png')
         # os.rename(output_path, os.path.join(image_dir, edit_prompt, 'styler.png'))
 print(i+1)
-with open('captions_imgs.txt', 'w') as f:
-    f.write(command)
+# with open('captions_imgs.txt', 'w') as f:
+#     f.write(command)
